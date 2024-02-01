18516ec5916fd8beee0d0f8ca69f13e2276ae6be
74079 <63118958+LukaHenriique@users.noreply.github.com>
2024-02-01 14:05:49 -0300

    [Aula - For If]

---
+++ ForIf/src/Aula01.java
@@ -0,0 +1,13 @@
+
+public class Aula01 {
+
+	public Aula01() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
